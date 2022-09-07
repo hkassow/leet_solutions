77568e887791b4a65791db828f8e021ad1360144
Hunter Kassow <hunterkassow@protonmail.com>
2022-09-07 10:11:24 -0400

    questions for plane

---
+++ questions.js
@@ -0,0 +1,362 @@
+// // 322. Coin Change
+// // Medium
+
+// // 13702
+
+// // 307
+
+// // Add to List
+
+// // Share
+// // You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// // Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// // You may assume that you have an infinite number of each kind of coin.
+
+// 238. Product of Array Except Self
+// Medium
+
+// 14390
+
+// 826
+
+// Add to List
+
+// Share
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+
+// 155. Min Stack
+// Medium
+
+// 9467
+
+// 670
+
+// Add to List
+
+// Share
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// Implement the MinStack class:
+
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+
+
+
+
+// 98. Validate Binary Search Tree
+// Medium
+
+// 12350
+
+// 999
+
+// Add to List
+
+// Share
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+// A valid BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+ 
+
+
+
+
+// 200. Number of Islands
+// Medium
+
+// 16809
+
+// 386
+
+// Add to List
+
+// Share
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+
+
+// 994. Rotting Oranges
+// Medium
+
+// 8187
+
+// 305
+
+// Add to List
+
+// Share
+// You are given an m x n grid where each cell can have one of three values:
+
+// 0 representing an empty cell,
+// 1 representing a fresh orange, or
+// 2 representing a rotten orange.
+// Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+// Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+ 
+
+
+// 33. Search in Rotated Sorted Array
+// Medium
+
+// 17263
+
+// 1042
+
+// Add to List
+
+// Share
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+
+
+
+
+// 39. Combination Sum
+// Medium
+
+// 13189
+
+// 268
+
+// Add to List
+
+// Share
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+// It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+
+
+// 46. Permutations
+// Medium
+
+// 12751
+
+// 216
+
+// Add to List
+
+// Share
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+
+// 56. Merge Intervals
+// Medium
+
+// 15943
+
+// 568
+
+// Add to List
+
+// Share
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+
+
+
+
+// 236. Lowest Common Ancestor of a Binary Tree
+// Medium
+
+// 12230
+
+// 304
+
+// Add to List
+
+// Share
+// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+
+
+
+
+
+// 981. Time Based Key-Value Store
+// Medium
+
+// 2514
+
+// 257
+
+// Add to List
+
+// Share
+// Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+// Implement the TimeMap class:
+
+// TimeMap() Initializes the object of the data structure.
+// void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+// String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+
+
+
+// 721. Accounts Merge
+// Medium
+
+// 4749
+
+// 834
+
+// Add to List
+
+// Share
+// Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+// Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+// After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+
+
+
+
+
+// 75. Sort Colors
+// Medium
+
+// 11978
+
+// 449
+
+// Add to List
+
+// Share
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// You must solve this problem without using the library's sort function.
+
+
+
+
+
+
+// 139. Word Break
+// Medium
+
+// 12093
+
+// 517
+
+// Add to List
+
+// Share
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+
+
+// 140. Word Break II
+// Hard
+
+// 5338
+
+// 487
+
+// Add to List
+
+// Share
+// Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+
+
+
+
+
+// 416. Partition Equal Subset Sum
+// Medium
+
+// 8771
+
+// 144
+
+// Add to List
+
+// Share
+// Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+
+
+
+
+
+
+
+
+// 8. String to Integer (atoi)
+// Medium
+
+// 2253
+
+// 6544
+
+// Add to List
+
+// Share
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+// The algorithm for myAtoi(string s) is as follows:
+
+// Read in and ignore any leading whitespace.
+// Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+// Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+// Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+// If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+// Return the integer as the final result.
+
+
+
